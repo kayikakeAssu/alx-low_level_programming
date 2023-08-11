67dbcbd277a897ea55dcde8c6c8bec9aa45dcea4
mbula assu <kayassurance@gmail.com>
2023-08-11 07:49:59 -0700

    if statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - srand, checks wheter a given random number is >,<or =0,
+ *                followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d", n);
+if (n > 0)
+printf("is positive\n");
+else if (n == 0)
+printf("is zero\n");
+else
+printf("is negative\n");
+return (0);
+}
