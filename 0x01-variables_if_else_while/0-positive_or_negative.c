0976e52d8b22699234c44111b811bd3b025dc923
mbula assu <kayassurance@gmail.com>
2023-08-11 08:34:20 -0700

    refactor1,2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,8 @@ printf("%d", n);
 if (n > 0)
 printf("is positive\n");
 else if (n == 0)
-printf("is zero\n");
+printf(" is zero\n");
 else
-printf("is negative\n");
+printf(" is negative\n");
 return (0);
 }
