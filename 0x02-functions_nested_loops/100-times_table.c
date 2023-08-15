0bfaeea6e0413d8d5bea73a931248d80e8490ff2
mbula assu <kayassurance@gmail.com>
2023-08-15 14:26:02 -0700

    tbla-100..

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,11 +11,9 @@ if (n < 0 || n > 15)
 {
 return;
 }
-int i;
-for (i = 0; i <= n; i++)
+for (int i = 0; i <= n; i++)
 {
-int j = 0;
-for (; j <= n; j++)
+for (int j = 0; j <= n; j++)
 {
 int product = i * j;
 if (j != 0)
