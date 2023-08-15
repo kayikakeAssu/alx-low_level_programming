51a1d9d5a2c070dd6d94a9ea33b1e584e3807109
mbula assu <kayassurance@gmail.com>
2023-08-15 12:51:58 -0700

    print-98..

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,10 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (int i = n; i <= 98; i++)
+for (; n <= 98; n++)
 {
-printf("%d", i);
-if (i != 98)
+printf("%d", n);
+if (n != 98)
 {
 printf(", ");
 }
@@ -21,10 +21,10 @@ printf(", ");
 }
 else
 {
-for (int i = n; i >= 98; i--)
+for (; n >= 98; n--)
 {
-printf("%d", i);
-if (i != 98)
+printf("%d", n);
+if (n != 98)
 {
 printf(", ");
 }
