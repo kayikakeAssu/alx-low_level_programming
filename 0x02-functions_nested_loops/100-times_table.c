d0614d2eaee159734e15edd0bd07f56461849a71
mbula assu <kayassurance@gmail.com>
2023-08-15 14:16:53 -0700

    tbla-100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,45 +7,41 @@
 */
 void print_times_table(int n)
 {
-if (n >= 0 && n <= 15)
+if (n < 0 || n > 15)
 {
+return;
+}
 int i = 0;
 for (; i <= n; i++)
 {
 int j = 0;
 for (; j <= n; j++)
 {
-int result = i * j;
-if (j > 0)
+int product = i * j;
+if (j != 0)
 {
+_putchar(',');
 _putchar(' ');
-}
-if (result < 10)
-{
-if (j > 0)
+if (product < 10)
 {
 _putchar(' ');
 _putchar(' ');
 }
-_putchar(result + '0');
-}
-else if (result < 100)
+else if (product < 100)
 {
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
+_putchar(' ');
+}
+}
+if (product < 10)
+{
+_putchar(product + '0');
 }
 else
 {
-_putchar((result / 100) + '0');
-_putchar(((result / 10) % 10) + '0');
-_putchar((result % 10) + '0');
-}
-if (j != n)
-{
-_putchar(',');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
 }
 }
 _putchar('\n');
-}
 }
 }
