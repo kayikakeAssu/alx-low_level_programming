e8b20cb80c3b9d350ff21bf80960fe1a29617af8
mbula assu <kayassurance@gmail.com>
2023-08-15 14:59:21 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Prints the first 52 fibonacci numbers
+*
+* Return:0
+*/
+int main(void)
+{
+int i;
+int fib[50];
+fib[0] = 1;
+fib[1] = 2;
+for (i = 2; i < 50; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+for (i = 0; i < 49; i++)
+{
+printf("%d, ", fib[i]);
+}
+printf("%d\n", fib[49]);
+return (0);
+}
