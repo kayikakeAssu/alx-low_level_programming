d331de059b21cded1c74ae9cb6b03b063f8f91ef
mbula assu <kayassurance@gmail.com>
2023-08-17 03:48:08 -0700

    remaining days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,21 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
-/**
-* positive_or_negative - srand, checks wheter a given number is >,<or =0,
-*                followed by a new line.
-*@i: the number
-* Return: Always 0.
-*/
-int positive_or_negative(int i)
-{
-printf("%d", i);
-if (i > 0)
-printf(" is positive\n");
-else if (i == 0)
-printf(" is zero\n");
-else
-printf(" is negative\n");
-return (0);
-}
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
