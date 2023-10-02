e2420a56dde062ba0fa1a67f1d3a3a4e62dad86a
kayikakeAssu <assurancekayikake4@gmail.com>
2023-10-02 04:52:00 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * create_file - ...
@@ -6,23 +6,23 @@
   * @text_content: ...
   *
   * Return: ...
-  */
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+int fd;
 
-	if (!filename)
-		return (-1);
+if (!filename)
+return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
 
 if (text_content)
-		write(fd, text_content, _strlen(text_content));
+write(fd, text_content, _strlen(text_content));
 
-	close(fd);
-	return (1);
+close(fd);
+return (1);
 }
 
 /**
